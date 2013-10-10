5a1c2643377e761009b075329ea784098421be37
Josh <joshbcondie@gmail.com>
2013-10-09 20:29:37 -0600

    Started on Lab 3. Changed the transformation methods so they use matrix concatenation instead of the translate and rotate methods

--- Lab 1/src/cs355/lab2/Transformation.java
+++ Lab 1/src/cs355/lab2/Transformation.java
@@ -6,17 +6,36 @@ import java.awt.geom.Point2D;
 public class Transformation {
 
 	public static Point2D objectToWorld(Shape shape, double x, double y) {
+
 		AffineTransform transform = new AffineTransform();
-		transform.translate(shape.getX(), shape.getY());
-		transform.rotate(shape.getRotation());
+
+		transform.concatenate(new AffineTransform(new double[] { 1, 0, 0, 1,
+				shape.getX(), shape.getY() }));
+
+		transform
+				.concatenate(new AffineTransform(new double[] {
+						Math.cos(shape.getRotation()),
+						Math.sin(shape.getRotation()),
+						-Math.sin(shape.getRotation()),
+						Math.cos(shape.getRotation()) }));
+
 		Point2D point = transform.transform(new Point2D.Double(x, y), null);
 		return point;
 	}
 
 	public static Point2D worldToObject(Shape shape, double x, double y) {
 		AffineTransform transform = new AffineTransform();
-		transform.rotate(-shape.getRotation());
-		transform.translate(-shape.getX(), -shape.getY());
+
+		transform
+				.concatenate(new AffineTransform(new double[] {
+						Math.cos(shape.getRotation()),
+						-Math.sin(shape.getRotation()),
+						Math.sin(shape.getRotation()),
+						Math.cos(shape.getRotation()) }));
+
+		transform.concatenate(new AffineTransform(new double[] { 1, 0, 0, 1,
+				-shape.getX(), -shape.getY() }));
+
 		Point2D point = transform.transform(new Point2D.Double(x, y), null);
 		return point;
 	}
